8f14ca898374b53d7e0a71fd936e1b391ccb043b
fzvincent <liangkun.yu@qq.com>
2019-03-04 14:34:03 +0800

    a

--- travel.cpp
+++ travel.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+
+
+#include <iostream>;
 struct travel_time
 {
     int hours;
     int mins;  
 };
 const int Mins_per_hr=60;
+travel_time sum(travel_time t1, travel_time t2 );
+void show_time (travel_time t);
+
+int main()
+{
+    using namespace std
+    travel_time day1={5,45};
+    travel_time day2={4,55};
+
+}
