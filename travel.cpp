494ecf40d7e36f606485dad7ddbb4713533e69ae
fzvincent <liangkun.yu@qq.com>
2019-03-04 16:57:15 +0800

    hello

--- travel.cpp
+++ travel.cpp
@@ -1,13 +1,8 @@
 
 
 #include <iostream>
-int main()
-{
-    using namespace std;
-    cout<<123;
-    getchar();
-}
-/* struct travel_time
+
+struct travel_time
 {
     int hours;
     int mins;  
@@ -18,9 +13,31 @@ void show_time (travel_time t);
 
 int main()
 {
-    using namespace std；
+    using namespace std;
     travel_time day1={5,45};
     travel_time day2={4,55};
-    cout >>1;
+    travel_time trip=sum(day1,day2);
+    cout<<"Two-day totoal: ";
+    show_time(trip);
 
-} */
+    travel_time day3={4,32};
+    cout<<"Three-day total: ";
+    show_time(sum(trip,day3));
+    
+
+getchar(  );
+}
+
+travel_time sum(travel_time t1,travel_time t2)
+{
+    travel_time total;
+    total.mins=(t1.mins+t2.mins)%Mins_per_hr;
+    total.hours=t1.hours+t2.hours+(t1.mins+t2.mins)/Mins_per_hr;
+    return total;
+}
+void show_time(travel_time t)
+{
+    using namespace std;
+    cout<<t.hours<<"hours,"<<t.mins<<"minuts\n";
+    
+}
